95160192362903399e1f81b6c27e20c6a0025426
soniagodhwani <soniaa.godhwani@gmail.com>
2020-06-24 21:45:35 -0400

    Initial commit for backend project structure

---
+++ virtualQueueAndOffers/src/main/java/com/visa/hackathon/virtualQueueAndOffers/VirtualQueueAndOffersApplication.java
@@ -0,0 +1,13 @@
+package com.visa.hackathon.virtualQueueAndOffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualQueueAndOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualQueueAndOffersApplication.class, args);
+	}
+
+}
---
+++ virtualQueueAndOffers/src/test/java/com/visa/hackathon/virtualQueueAndOffers/VirtualQueueAndOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visa.hackathon.virtualQueueAndOffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualQueueAndOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
