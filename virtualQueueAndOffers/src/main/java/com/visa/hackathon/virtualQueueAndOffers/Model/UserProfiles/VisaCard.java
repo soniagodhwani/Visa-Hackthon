dcbd0bff8a4070e8bef6462e222453d48db6d14d
GloriaGilbertNazareth <glorianazareth94@gmail.com>
2020-06-25 14:41:36 -0400

    Update VisaCard Model as per API Save card

--- virtualQueueAndOffers/src/main/java/com/visa/hackathon/virtualQueueAndOffers/Model/UserProfiles/VisaCard.java
+++ virtualQueueAndOffers/src/main/java/com/visa/hackathon/virtualQueueAndOffers/Model/UserProfiles/VisaCard.java
@@ -19,6 +19,21 @@ public class VisaCard {
     private String cardNumber;
 
     @NotNull
+    private String cvv;
+    
+    @NotNull
+    private String billingZipCode;
+    
+    @NotNull
+    private Integer expirationMonth;
+    
+    @NotNull
+    private Integer expirationYear;
+    
+    @NotNull
+    private String nameOnCard;
+    
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "CUSTOMER_ID")
     private Customer customer;
